d26362aba737815a8f5920d7471cdc4bca39f1bc
Pranjal Bhardwaj <pranjalb21@iitk.ac.in>
2024-03-10 03:48:27 +0530

    _inti_

--- q_learning.py
+++ q_learning.py
@@ -5,6 +5,9 @@ from Envs import rlenv
 from utils import *
 import pickle 
 import time
+import sys
+import os
+from tqdm import tqdm
 from colorama import Fore , Back
 
 def run(EPISODES,verbose,epsilon_value,print_val,q,env):
@@ -12,17 +15,28 @@ def run(EPISODES,verbose,epsilon_value,print_val,q,env):
     learning_rate=0.7
     discount_factor=1
     epsilon= epsilon_value ## 100% random actions
-    epsilon_decay_rate=0.0001
+    epsilon_decay_rate=0.000004
     rng = np.random.default_rng()
     reward_per_episodes=np.zeros(EPISODES)
     # env= rlenv()
     
-    for i in range (EPISODES):
-        if (i %print_val==0):
-            print(i, epsilon)
+    # Load Q-table from a file
+    # def load_q_table(filename):
+    #     with open(filename, 'rb') as f:
+    #         Q = pickle.load(f)
+    #     return Q
+
+    def save_q_table(Q, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(Q, f)
+
+    for i in tqdm(range(EPISODES)):
+        
         state,_ = env.reset()
         done= False
         truncated= False
+        if (i%1000==0):
+            print(i)
         while (not done and  not truncated):
             # print("*********************",state[0])
             if rng.random() <epsilon:
@@ -39,14 +53,43 @@ def run(EPISODES,verbose,epsilon_value,print_val,q,env):
             reward_per_episodes[i]+=reward
             # print(q)
             # break
-        epsilon= max(epsilon-epsilon_decay_rate,0)
-    print(q)
+        epsilon= max(epsilon-epsilon_decay_rate,0.15)
+        if (i %print_val==0):
+            print(i, epsilon)
+        ##------idhar bhi save wali cheez daalni hai------##
+            #Q = sys.argv[1] # Your Q-table
+           # save_q_table(Q, 'q_table.pkl')     
+        #----yahan tak----#
+            print(f"Truncated {truncated} DONE {done}")
+            print_grid_and_path(env.grid,env.state_trajectory ,conf=None,save_path='Q-learning/', plotting=False)
+        
+        ##---------edit karna hai----------#
+        # pbar_outer.set_description(f"Episode {i}")
+        # pbar_outer.update(1)
+        # break
+        
+    #save line daalni hai#
+    #Q = sys.argv[1] # Your Q-table
+    #save_q_table(Q, 'q_table.pkl')          
+    # print(q)
 
 def main():
     print("a")
     env = rlenv()
-    q = np.zeros((env.W*env.H,env.action_space.n))
-    run(11000,False,1,1000,q,env)
+    if len(sys.argv) >1 :
+        q_table_filename = sys.argv[1]
+        # q = np.load(q_table_filename)
+        if os.path.exists(q_table_filename):
+            with open(q_table_filename, 'rb') as f:
+                q = pickle.load(f)
+        else:
+            q = np.zeros((env.W*env.H,env.action_space.n))
+            with open(q_table_filename, 'wb') as f:
+                pickle.dump(q, f)
+    else:
+        q = np.zeros((env.W*env.H,env.action_space.n))
+
+    run(250000,False,1,10000,q,env,filename)
     ## get values of system argvv
 
 
